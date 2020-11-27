10915d4fc3be908f96a4d922bd0f05acb363119b
Michel R. Jean <mrjean@videotron.ca>
2020-11-27 14:11:18 -0500

    Adding the views

---
+++ Models/category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogEngine.Models
+{
+    public class category
+    {
+        public string title { get; set; }
+    }
+}
---
+++ Models/post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogEngine.Models
+{
+    public class post
+    {
+        public string title { get; set; }
+
+        public DateTime publicationDate { get; set; }
+        public string content { get; set; }
+    }
+}
